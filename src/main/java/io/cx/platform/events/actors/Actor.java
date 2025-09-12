8877efb2f014d02c2c6698b0f210ef550003be31
sergey <s.shvyrev@mail.ru>
2025-09-12 16:22:02 +0300

    upd

--- src/main/java/io/cx/platform/events/actors/Actor.java
+++ src/main/java/io/cx/platform/events/actors/Actor.java
@@ -1,15 +1,28 @@
 package io.cx.platform.events.actors;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.Accessors;
+
+import java.util.Objects;
+import java.util.stream.Stream;
 
 @Getter
 @Setter
 @Accessors(chain = true)
 @EqualsAndHashCode
 @AllArgsConstructor
-@ToString
 public class Actor {
     private final ActorType type;
     private final String name;
+
+    @Override
+    public String toString() {
+        return Stream.of(getType(), getName())
+                .filter(Objects::nonNull)
+                .map(v -> v.toString().trim().toLowerCase())
+                .reduce("", (a, b) -> a + ":" + b);
+    }
 }
