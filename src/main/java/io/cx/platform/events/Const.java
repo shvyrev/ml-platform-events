88a22f08b33308faf1a42a62eeae2ec59ad479c8
sergey <s.shvyrev@mail.ru>
2025-09-23 11:17:16 +0300

    upd

---
+++ src/main/java/io/cx/platform/events/Const.java
@@ -0,0 +1,5 @@
+package io.cx.platform.events;
+
+public class Const {
+    public static final String GI_2 = "2Gi";
+}
--- src/main/java/io/cx/platform/events/resources/endpoint/EndpointCreated.java
+++ src/main/java/io/cx/platform/events/resources/endpoint/EndpointCreated.java
@@ -2,6 +2,7 @@ package io.cx.platform.events.resources.endpoint;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.cx.platform.events.dto.InfraResources;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,9 @@ import lombok.experimental.Accessors;
 
 import java.util.UUID;
 
+import static io.cx.platform.events.Const.GI_2;
+import static java.util.Optional.ofNullable;
+
 @Getter
 @Setter
 @Accessors(chain = true)
@@ -17,6 +21,26 @@ import java.util.UUID;
 @ToString
 public final class EndpointCreated extends EndpointEvents {
     private String userId;
+    private String schSizeLimit = GI_2;
+    private InfraResources requestedResources = new InfraResources();
+    private InfraResources limitedResources = new InfraResources();
+
+    @JsonCreator
+    public EndpointCreated(
+            @JsonProperty("endpointId") UUID endpointId,
+            @JsonProperty("userId") String userId,
+            @JsonProperty("schSizeLimit") String schSizeLimit,
+            @JsonProperty("request") InfraResources requestedResources,
+            @JsonProperty("limit") InfraResources limitedResources) {
+        super(endpointId);
+        this.userId = userId;
+        this.schSizeLimit = ofNullable(schSizeLimit)
+                .orElse(GI_2);
+        this.requestedResources = ofNullable(requestedResources)
+                .orElse(new InfraResources());
+        this.limitedResources = ofNullable(limitedResources)
+                .orElse(new InfraResources());
+    }
 
     @JsonCreator
     public EndpointCreated(
