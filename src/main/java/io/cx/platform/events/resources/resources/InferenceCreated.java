7454758e457835392187aef20cbe1ea12de496aa
sergey <s.shvyrev@mail.ru>
2025-09-24 08:48:35 +0300

    upd

--- src/main/java/io/cx/platform/events/resources/resources/InferenceCreated.java
+++ src/main/java/io/cx/platform/events/resources/resources/InferenceCreated.java
@@ -20,6 +20,7 @@ public final class InferenceCreated extends ResourceEvents {
     private UUID model;
     private UUID version;
     private UUID endpoint;
+    private String url;
 
     public InferenceCreated(ServingResourceInfo info) {
         super(info);
@@ -30,10 +31,12 @@ public final class InferenceCreated extends ResourceEvents {
             @JsonProperty("model") UUID model,
             @JsonProperty("version") UUID version,
             @JsonProperty("endpoint") UUID endpoint,
+            @JsonProperty("url") String url,
             @JsonProperty("info") ServingResourceInfo info) {
         super(info);
         this.model = model;
         this.version = version;
         this.endpoint = endpoint;
+        this.url = url;
     }
 }
--- src/main/java/io/cx/platform/events/resources/resources/InferenceDeleted.java
+++ src/main/java/io/cx/platform/events/resources/resources/InferenceDeleted.java
@@ -21,6 +21,7 @@ public final class InferenceDeleted extends ResourceEvents {
     private UUID model;
     private UUID version;
     private UUID endpoint;
+    private String url;
 
     public InferenceDeleted(ServingResourceInfo info) {
         super(info);
@@ -31,10 +32,12 @@ public final class InferenceDeleted extends ResourceEvents {
             @JsonProperty("model") UUID model,
             @JsonProperty("version") UUID version,
             @JsonProperty("endpoint") UUID endpoint,
+            @JsonProperty("url") String url,
             @JsonProperty("info") ServingResourceInfo info) {
         super(info);
         this.model = model;
         this.version = version;
         this.endpoint = endpoint;
+        this.url = url;
     }
 }
