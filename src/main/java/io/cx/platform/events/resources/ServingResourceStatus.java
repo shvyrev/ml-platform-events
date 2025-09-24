f179e2e79cff620242f8ccde0d9db8cfcdd983e6
sergey <s.shvyrev@mail.ru>
2025-09-24 07:39:47 +0300

    upd

--- src/main/java/io/cx/platform/events/resources/ServingResourceStatus.java
+++ src/main/java/io/cx/platform/events/resources/ServingResourceStatus.java
@@ -2,6 +2,10 @@ package io.cx.platform.events.resources;
 
 public enum ServingResourceStatus {
     PENDING,
+    STANDBY,
+    LOADING,
+    LOADED,
+    FAILEDTOLOAD,
     FAILED,
     DELETED,
     READY;
