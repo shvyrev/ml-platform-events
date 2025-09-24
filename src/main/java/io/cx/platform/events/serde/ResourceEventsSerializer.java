e9f471188166089ea2a87c1c484a530b7de21d82
sergey <s.shvyrev@mail.ru>
2025-09-24 15:24:41 +0300

    upd

---
+++ src/main/java/io/cx/platform/events/serde/ResourceEventsSerializer.java
@@ -0,0 +1,19 @@
+package io.cx.platform.events.serde;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import io.cx.platform.events.resources.resources.ResourceEvents;
+import org.apache.kafka.common.serialization.Serializer;
+
+public class ResourceEventsSerializer implements Serializer<ResourceEvents> {
+    private final ObjectMapper objectMapper = new ObjectMapper()
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    @Override
+    public byte[] serialize(String topic, ResourceEvents data) {
+        try {
+            return objectMapper.writeValueAsBytes(data);
+        } catch (Exception e) {
+            throw new RuntimeException("Error serializing ResourceEvents", e);
+        }
+    }
+}
