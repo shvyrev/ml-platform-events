78396a2afb6145ec095a4d9c38bed2c4c88d4ca7
sergey <s.shvyrev@mail.ru>
2025-09-26 13:06:11 +0300

    upd

--- src/main/java/io/cx/platform/events/resources/model/ModelRegistered.java
+++ src/main/java/io/cx/platform/events/resources/model/ModelRegistered.java
@@ -2,6 +2,7 @@ package io.cx.platform.events.resources.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.cx.platform.events.dto.InferenceParameters;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,11 +18,15 @@ import java.util.UUID;
 @ToString
 public final class ModelRegistered extends ModelEvents {
     private String userId;
+    private InferenceParameters inferenceParameters;
 
     @JsonCreator
-    public ModelRegistered(@JsonProperty("modelId") UUID modelId, @JsonProperty("userId") String userId) {
+    public ModelRegistered(@JsonProperty("modelId") UUID modelId,
+                           @JsonProperty("userId") String userId,
+                           @JsonProperty("inferenceParameters") InferenceParameters inferenceParameters) {
         super(modelId);
         this.userId = userId;
+        this.inferenceParameters = inferenceParameters;
     }
 
 }
